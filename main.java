ad44c221df54ea78057f60671b8cf089c7de12a0
jahnavi0612 <22jr1a4209@gmail.com>
2025-06-06 09:52:26 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,62 +1,35 @@
 import java.util.*;
-
-public class main {
-    static void flip(int[] arr, int k) {
-        int left = 0, right = k;
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
+class person{
+    protected String name;
+    protected void setname(String name){
+        this.name=name;
+}
+protected void displayname(){
+    System.out.println("Name: "+name);
+}
+}
+class boy extends person{
+    protected float height;
+    protected void setheight(float height){
+        this.height=height;
     }
-
-    static int findmaxindex(int[] arr, int n) {
-        int maxindex = 0;
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > arr[maxindex]) 
-	   {
-                maxindex = i;
-            }
-        }
-        return maxindex;
+    public void displaydetails(){
+        displayname();
+        System.out.println("height: "+height);
     }
-
-    static void pancakesort(int[] arr, int n) {
-        for (int size = n; size > 1; size--) {
-            int maxindex = findmaxindex(arr, size);
-            if (maxindex != size - 1) {
-                if (maxindex > 0) {
-                    flip(arr, maxindex);
-                }
-                flip(arr, size - 1);
-            }
-        }
-    }
-
-    static void printarray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n\nEnter the number of elements: \n");
-        int n = scanner.nextInt();
-
-        int[] arr = new int[n];
-        System.out.println("Enter the elements: \n");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-
-        pancakesort(arr, n);
-        System.out.println("\nSorted array: \n");
-        printarray(arr);
-
-        scanner.close();
+}
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        boy b=new boy();
+        System.out.println("Enter the name: ");
+        String nameinput=sc.nextLine();
+        b.setname(nameinput);
+        System.out.println("Enter height: ");
+        float height=sc.nextFloat();
+        b.setheight(height);
+        System.out.println("----------person details---------");
+        b.displaydetails();
     }
 }
